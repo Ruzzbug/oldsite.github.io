7b5a9f0b3f5d8986757fd33320e8d455880efec7
Ryan V <ryandvd2000@gmail.com>
2022-05-31 17:40:02 +1000

    Sidebar V1 Finished (No colours)

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,15 +1,18 @@
 (function () {
-    'use strict'
-    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-    tooltipTriggerList.forEach(function (tooltipTriggerEl) {
-      new bootstrap.Tooltip(tooltipTriggerEl)
-    })
-  })()
+  "use strict";
+  var tooltipTriggerList = [].slice.call(
+    document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  );
+  tooltipTriggerList.forEach(function (tooltipTriggerEl) {
+    new bootstrap.Tooltip(tooltipTriggerEl);
+  });
+})();
 
-function sideTransition (element){
-  if(element.ariaExpanded === "false") {
-    element.parentNode.style.backgroundColor = "var(--tertiary)";
-  } else {
-    element.parentNode.style.backgroundColor = "var(--accent)";
+function sideTransition(element) {
+  if (element.ariaExpanded === "false") {
+    element.parentNode.style.removeProperty("background-color");
+    return;
   }
+  element.parentNode.style.backgroundColor = "var(--accent)";
+  return;
 }
