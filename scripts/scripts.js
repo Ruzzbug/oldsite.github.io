43f9e756535dab9f92690fd45c65d2edd7796e71
Ryan V <ryandvd2000@gmail.com>
2022-06-01 17:23:20 +1000

    Sidebar Mobile Working

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -8,6 +8,7 @@
   });
 })();
 
+//Change colour on dropdown
 function sideTransition(element) {
   if (element.ariaExpanded === "false") {
     element.parentNode.style.removeProperty("background-color");
@@ -16,3 +17,32 @@ function sideTransition(element) {
   element.parentNode.style.backgroundColor = "var(--accent)";
   return;
 }
+
+var open = false;
+//Open and close sidebar
+function toggleNav() {
+  if (open) {
+    document.getElementById("sidebar").style.width = "0";
+    document.getElementById("content-container").style.filter = "none";
+    document.body.style.backgroundColor = "var(--primary)";
+    document.getElementById("navTog").classList.remove("opened");
+    document.getElementById("navTog").classList.add("closed");
+    open = false;
+    return;
+  }
+  document.getElementById("sidebar").style.width = "var(--sidebar-width)";
+  document.getElementById("content-container").style.filter = "blur(5px)";
+  document.body.style.overflowX = "hidden";
+  document.body.style.backgroundColor = "#888888"; //should use a div that's above to also darken images
+  document.getElementById("navTog").classList.remove("closed");
+  document.getElementById("navTog").classList.add("opened");
+  document.getElementById("content-container").addEventListener("click", onClickOutside);
+  open = true;  
+}
+
+const onClickOutside = (e) => {
+  if (!e.target.className.includes("sidebar")) {
+    document.getElementById("content-container").removeEventListener("click", onClickOutside);
+    toggleNav();
+  }
+};
